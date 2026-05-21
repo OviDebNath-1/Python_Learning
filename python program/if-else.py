931fd24348837781b0b0cd09c26c7b00ccdc34e1
OviDebNath-4 <ovinth2@gmail.com>
2026-05-21 17:34:19 +0600

    first commit

---
+++ python program/if-else.py
@@ -0,0 +1,54 @@
+#solve problem-1
+
+# num1=int(input("Enter first number: "))
+# num2=int(input("Enter second number: "))
+
+# if num1>num2:
+#     print(f"{num1} is greater than {num2}")
+# elif num1<num2:
+#     print(f"{num2} is greater than {num1}")
+# else:
+#     print("Both numbers are equal")
+
+# solve problem-2
+# gen=input("please tell your gender(M or F): ")
+# if gen == 'M' or gen == 'm':
+#     print("Hello Sir")
+# elif gen == 'F' or gen == 'f':
+#     print("Hello Mam")
+# else:
+#     print("others")
+
+# solve problem-3
+# a=int(input("please tell your number: "))
+# if a%2==0:
+#     print(f"{a} is even number")
+# else:
+#     print(f"{a} is odd number")
+# solve problem-4
+# name=input("please tell your name: ")
+# age=int(input("please tell your age : "))
+# if age>=18:
+#     print(f"Hello {name}, you are eligible for vote")
+# else:
+#     print(f"Hello {name}, you can vote after {18-age} years")
+
+# solve problem-5
+# year=int(input("please tell the year: "))
+
+# if year%400==0 or(year%4==0 and year%100!=0):
+#     print(f"{year} is a leap year")
+    
+# else:
+#     print(f"{year} is not a leap year")
+# solve problem-6
+
+# temp=int(input("please tell the temperature: "))
+# if temp>=-5 and temp<=5:
+#     print("The weather is very cold")
+# elif temp>=6 and temp<=18:
+#     print("The weather is cold")
+# elif temp>=19 and temp<=30:
+#     print("The weather is hot")
+# else:
+#     print("The weather is very hot")
